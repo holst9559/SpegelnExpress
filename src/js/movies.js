453c71f3a96697a4e3bb573c3233bfbb03845c9e
yvette809 <60625477+yvette809@users.noreply.github.com>
2022-12-22 23:39:03 +0100

    movies mapped

--- src/js/movies.js
+++ src/js/movies.js
@@ -1,3 +1,94 @@
+import  movies  from   '../data/movies.json' assert { type: 'json' };
+
+console.log('movies', movies) 
+
+let moviesList = document.querySelector('.movies-now')
+let futureMovies = document.querySelector('.movies-later ')
+let heading = document.querySelector('.heading-1')
+
+function displayMovies (){
+   
+    movies.movies.map(movie=>{
+      
+        for(let m of movie.screeningTimes){
+           
+            console.log('m',new Date(m))
+            console.log('new date',  new Date().toISOString().split("T")[0])
+            if(new Date(m).toISOString().split("T")[0] ===  new Date().toISOString().split("T")[0]){
+
+                // get the heading
+                let heading = document.querySelector('.heading-1')
+                heading.innerText = 'På Bion Just Nu'   
+
+            }
+        }
+
+                let heading = document.querySelector('.heading-2')
+                heading.innerText = 'kommer snårt'
+               
+                 // create movie card
+             let movieCard = document.createElement('div')
+              movieCard.className = 'movies'
+
+           // create image div
+           let imgDiv = document.createElement('div')
+           imgDiv.classList.add('img-div')
+
+           // create image
+           let image = document.createElement('img')
+           image.src = movie.imageUrl
+
+            imgDiv.appendChild(image)
+
+           // create title
+          let title = document.createElement('h1')
+         title.innerText = `${movie.title}`
+         movieCard.appendChild(imgDiv)
+         movieCard.appendChild(title)
+      
+
+        futureMovies.appendChild(movieCard)
+       
+          
+       
+    })
+}
+
+function createMovieCard(movie){
+       // create movie card
+       let movieCard = document.createElement('div')
+       movieCard.className = 'movies'
+
+       // create image div
+       let imgDiv = document.createElement('div')
+       imgDiv.classList.add('img-div')
+
+       // create image
+       let image = document.createElement('img')
+       image.src = movie.imageUrl
+
+       imgDiv.appendChild(image)
+
+       // create title
+       let title = document.createElement('h1')
+       title.innerText = `${movie.title}`
+       movieCard.appendChild(imgDiv)
+       movieCard.appendChild(title)
+        movieCard
+      
+       moviesList.appendChild(movieCard)
+    
+
+}
+
+displayMovies()
+
+
+
+
+
+
+
 //Hantera startsidans filmer
 
 //klass som hanterar renderandet av alla filmer ("MovieList")
