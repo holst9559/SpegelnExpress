fd23863a1f291b224a40be8850010c70767aefbb
Axel Asp <solidtop.asp@gmail.com>
2023-01-02 11:13:52 +0100

    Add api adapter

--- src/js/api.js
+++ src/js/api.js
@@ -1,3 +1,16 @@
-//API adapter. Här hämtas all json data
+export default class APIAdapter {
+
+    async fetchMovies() {
+        const res = await fetch("./src/data/movies.json");
+        const data = await res.json();
+        return data.movies;
+    }
+
+    async fetchMenu() {
+        const res = await fetch("./src/data/menu.json");
+        const data = await res.json();
+        return data.food;
+    }
+}
 
 
