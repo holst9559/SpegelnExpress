dccb7793639ad289713c749105104ff5eb7e93bd
Axel Asp <solidtop.asp@gmail.com>
2023-01-02 14:29:25 +0100

    Resolve conflict

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,15 @@
 import NavMenuTrigger from "./navMenu.js";
+import UrlHandlerClass from "./router.js";
 import APIAdapter from "./api.js";
+
+new NavMenuTrigger();
+new UrlHandlerClass();
 
 const api = new APIAdapter();
 const movies = await api.fetchMovies();
 const menu = await api.fetchMenu();
 
-new NavMenuTrigger();
+
 
 
 
