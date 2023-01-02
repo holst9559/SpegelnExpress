77d9f4ddfed2da45e6555a3e3fd37b5f88265d81
Axel Asp <solidtop.asp@gmail.com>
2023-01-02 11:14:18 +0100

    import api class into main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 import NavMenuTrigger from "./navMenu.js";
+import APIAdapter from "./api.js";
+
+const api = new APIAdapter();
+const movies = await api.fetchMovies();
+const menu = await api.fetchMenu();
 
 new NavMenuTrigger();
+
+
+
