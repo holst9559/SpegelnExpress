d113540d7728eca6278f65cdc84ecdb3af0db0c1
Anton Holst <holst9559@hotmail.com>
2023-01-25 17:59:50 +0100

    Wrote function to fetch API data, rewrote html into handlebars, used my old router in making the express router. Also added new handlbars files to handle the rendering of API data.

---
+++ src/data/moviesData.js
@@ -0,0 +1,15 @@
+import fetch from "node-fetch";
+
+const APIData = "https://plankton-app-xhkom.ondigitalocean.app/api"
+
+export async function getMovies() {
+    const res = await fetch(APIData + "/movies")
+    const content = await res.json();
+    return content.data;
+}
+
+export async function getMovie(id) {
+    const res = await fetch(APIData + "/movies/" + id);
+    const content = await res.json();
+    return content.data;
+}
