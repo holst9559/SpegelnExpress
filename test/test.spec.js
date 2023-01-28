1ec123003f9a9e477a67bd3e33a1954f6a67cb30
Anton Holst <holst9559@hotmail.com>
2023-01-28 11:44:54 +0100

    Changed some line of code which checks if the correct title is displayed

--- test/test.spec.js
+++ test/test.spec.js
@@ -10,7 +10,7 @@ test("encanto test", async () => {
         .get("/movie/2")
         .expect("Content-type", "text/html; charset=utf-8")
         .expect(200);
-    expect(response.text.includes(movies[0].attributes.title)).toBeTruthy();
+    expect(response.text).toMatch(movies[0].attributes.title);
     expect(response.text.includes("Encanto")).toBeTruthy();
 });
 
@@ -19,7 +19,7 @@ test("isle of dogs test", async () => {
         .get("/movie/1")
         .expect("Content-type", "text/html; charset=utf-8")
         .expect(200);
-    expect(response.text.includes(movies[1].attributes.title)).toBeTruthy();
+    expect(response.text).toMatch(movies[1].attributes.title);
     expect(response.text.includes("Isle of dogs")).toBeTruthy();
 });
 
@@ -28,7 +28,7 @@ test("shawshank test", async () => {
         .get("/movie/3")
         .expect("Content-type", "text/html; charset=utf-8")
         .expect(200);
-    expect(response.text.includes(movies[3].attributes.title)).toBeTruthy();
+    expect(response.text).toMatch(movies[3].attributes.title);
     expect(response.text.includes("The Shawshank Redemption")).toBeTruthy();
 });
 
@@ -37,13 +37,13 @@ test("totoro test", async () => {
         .get("/movie/4")
         .expect("Content-type", "text/html; charset=utf-8")
         .expect(200)        
-    expect(response.text.includes(movies[2].attributes.title)).toBeTruthy();
+    expect(response.text).toMatch(movies[2].attributes.title);
     expect(response.text.includes("Min granne Totoro")).toBeTruthy();
 });
 
 test("404 test", async () => {
     const response = await request(app)
-        .get("/movie/5")
+        .get("/movie/42")
         .expect(404);
     expect(response.text.includes("404 NOT FOUND"));
 })
